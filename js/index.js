d0320b40811e7d60f89aeccb2c1f6d4cf8cfb772
João Carlos Ferreira Neto <58020444+JoaoCFN@users.noreply.github.com>
2020-04-17 23:13:50 -0300

    validação do CEP

--- js/index.js
+++ js/index.js
@@ -1,22 +1,26 @@
-const cep = document.querySelector("input#cep");
-const btn_buscar = document.querySelector(".btn-buscar");
-
-btn_buscar.addEventListener("click", () => {
-    const cep_conv = "44032582";//String(cep.value);
+const btn_buscar = document.querySelector(".btn-buscar").addEventListener("click", () => {
+    const cep = document.querySelector("input#cep");
     const titulo = document.querySelector(".titulo");
     const rua = document.querySelector(".rua");
     const bairro = document.querySelector(".bairro");
     const cidade = document.querySelector(".cidade_estado");
-    const estado = document.querySelector(".estado");
+    const msg_error = document.querySelector("div.msg_cep");
+    msg_error.innerHTML = ``;
 
-    fetch(`https://viacep.com.br/ws/${cep_conv}/json`)
-    .then(response => response.json())
-    .then(data => {
-        titulo.innerHTML = `<i class="fa fa-street-view pr-2"></i>Endereço`;
-        rua.innerHTML = `${data.logradouro}`;
-        bairro.innerHTML = `${data.bairro}`;
-        cidade.innerHTML = `${data.localidade} - ${data.uf}`;
-    })
+    if(cep.value.length > 8){
+        msg_error.innerHTML = `Digite um CEP de 8 dígitos`;
+    }
+    else{
+        const cep_conv = String(cep.value);
+        fetch(`https://viacep.com.br/ws/${cep_conv}/json`)
+        .then(response => response.json())
+        .then(data => {
+            titulo.innerHTML = `<i class="fa fa-street-view pr-2"></i>Endereço`;
+            rua.innerHTML = `${data.logradouro}`;
+            bairro.innerHTML = `${data.bairro}`;
+            cidade.innerHTML = `${data.localidade} - ${data.uf}`;
+        })
+    }
 })
 
 
