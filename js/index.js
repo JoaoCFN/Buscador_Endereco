631a2799853712d171f5fca5771a6c6ca527f962
João Carlos Ferreira Neto <58020444+JoaoCFN@users.noreply.github.com>
2020-04-17 23:39:21 -0300

    validação do campo CEP do buscador

--- js/index.js
+++ js/index.js
@@ -7,19 +7,28 @@ const btn_buscar = document.querySelector(".btn-buscar").addEventListener("click
     const msg_error = document.querySelector("div.msg_cep");
     msg_error.innerHTML = ``;
 
-    if(cep.value.length > 8){
-        msg_error.innerHTML = `Digite um CEP de 8 dígitos`;
-    }
-    else{
+    if(cep.value.length == 8){
         const cep_conv = String(cep.value);
         fetch(`https://viacep.com.br/ws/${cep_conv}/json`)
         .then(response => response.json())
         .then(data => {
-            titulo.innerHTML = `<i class="fa fa-street-view pr-2"></i>Endereço`;
-            rua.innerHTML = `${data.logradouro}`;
-            bairro.innerHTML = `${data.bairro}`;
-            cidade.innerHTML = `${data.localidade} - ${data.uf}`;
-        })
+            /* Se rua for undefined, todos os outros serão também. 
+               logo o CEP é inválido ou não consta na base de dados do via CEP
+            */
+            if (data.logradouro == undefined){
+                msg_error.innerHTML = `O CEP não existe`;
+            }
+            else{
+                titulo.innerHTML = `<i class="fa fa-street-view pr-2"></i>Endereço`;
+                rua.innerHTML = `${data.logradouro}`;
+                bairro.innerHTML = `${data.bairro}`;
+                cidade.innerHTML = `${data.localidade} - ${data.uf}`;
+            }
+            
+        })     
+    }  
+    else{
+        msg_error.innerHTML = `Digite um CEP de 8 dígitos`;
     }
 })
 
